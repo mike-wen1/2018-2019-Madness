d4e117772c49ccd9af09887e9b1e029e7c4e93ba
wuivy <ivy.wu@students.tka.org>
2018-10-27 15:25:39 -0700

    Sphere side basic

---
+++ autonomous/SphereSide.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+public class SphereSide extends BaseAutonomous {
+    public void runOpMode() {
+        initialize();
+
+        holdMarker();
+
+        waitForStart();
+
+        moveBot(1000, 1);
+    }
+}
